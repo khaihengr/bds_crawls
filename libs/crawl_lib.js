18952fe8e059fc57ad659893cf8cbb5d94b2f26d
Khai Hoang <khaihoangdev@gmail.com>
2018-04-05 11:28:45 +0700

    update compare string

--- libs/crawl_lib.js
+++ libs/crawl_lib.js
@@ -5,13 +5,12 @@ const _ = require("lodash");
 const { add } = require("../controllers/c_property");
 const moment = require("moment");
 const conn = require('../database/mysql');
-console.log("ok")
 const request = require("request");
 
 // conn.test_conn();
 let cities = ['An Giang', 'Bà Rịa - Vũng Tàu', 'Bắc Giang', 'Bắc Kạn', 'Bạc Liêu', 'Bắc Ninh', 'Bến Tre', 'Bình Định', 'Bình Dương', 'Bình Phước', 'Bình Thuận', 'Cà Mau', 'Cao Bằng', 'Đắk Lắk', 'Đắk Nông', 'Điện Biên', 'Đồng Nai', 'Đồng Tháp', 'Gia Lai', 'Hà Giang', 'Hà Nam', 'Hà Tĩnh', 'Hải Dương', 'Hậu Giang', 'Hòa Bình', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang', 'Kon Tum', 'Lai Châu', 'Lâm Đồng', 'Lạng Sơn', 'Lào Cai', 'Long An', 'Nam Định', 'Nghệ An', 'Ninh Bình', 'Ninh Thuận', 'Phú Thọ', 'Quảng Bình', 'Quảng Nam', 'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sóc Trăng', 'Sơn La', 'Tây Ninh', 'Thái Bình', 'Thái Nguyên', 'Thanh Hóa', 'Thừa Thiên Huế', 'Tiền Giang', 'Trà Vinh', 'Tuyên Quang', 'Vĩnh Long', 'Vĩnh Phúc', 'Yên Bái', 'Phú Yên', 'Cần Thơ', 'Đà Nẵng', 'Hải Phòng', 'Hà Nội', 'TP HCM'];
 let categories = ['Bán căn hộ chung cư', 'Bán kho, nhà xưởng, đất công nghiệp', 'Bán loại bất động sản khác', 'Bán nhà biệt thự', 'liền kề, phân lô', 'Bán nhà mặt phố', 'Bán nhà riêng', 'Bán sàn văn phòng thương mại', 'Bán trang trại', 'khu nghỉ dưỡng', 'Bán đất', 'Bán đất nền dự án'];
-let units = ['Thỏa thuận', 'Triệu', 'Tỷ', 'Trăm nghìn/m2', 'Triệu/m2','Trăm nghìn/tháng','Triệu/tháng','Trăm nghìn/m2/tháng','Triệu/m2/tháng','Nghìn/m2/tháng'];
+let units = ['Thỏa thuận', 'Triệu', 'Tỷ', 'Trăm nghìn/m²', 'Triệu/m²','Trăm nghìn/tháng','Triệu/tháng','Trăm nghìn/m²/tháng','Triệu/m²/tháng','Nghìn/m²/tháng'];
 
 let getPosts = (link, form_set) => {
     return new Promise((resolve)=>{
